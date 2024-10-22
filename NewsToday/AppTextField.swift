e15097a1389c0c247933f298d009a8aee05ed999
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-10-22 23:12:55 +0500

    реализовал регистрацию и перенес категории в отдельный файл

--- NewsToday/AppTextField.swift
+++ NewsToday/AppTextField.swift
@@ -14,17 +14,15 @@ struct AppTextField: View {
     
     let placeholder: String
     let imageName: String
+    
     var isSecure: Bool = false
     
     var body: some View {
         HStack {
             Image(systemName: imageName)
-                .foregroundColor(isFocused ? .blue : .gray)
+                .foregroundColor(isFocused ? .purplePrimary : .greyPrimary)
             if isSecure {
-                SecureField(placeholder, text: $textFieldText)
-                    .focused($isFocused)
-                    .textInputAutocapitalization(.never)
-                    .autocorrectionDisabled(true)
+                SecureTextField(textFieldText: $textFieldText, placeholder: placeholder)
             } else {
                 TextField(placeholder, text: $textFieldText)
                     .focused($isFocused)
@@ -33,13 +31,40 @@ struct AppTextField: View {
             }
         }
         .padding()
-        .background(isFocused ? .clear : Color(.systemGray6))
+        .background(isFocused ? .clear : .greyLighter)
         .overlay {
             if isFocused {
                 RoundedRectangle(cornerRadius: 12)
-                    .stroke(Color.blue, lineWidth: 1)
+                    .stroke(Color(.purplePrimary), lineWidth: 1)
             }
         }
         .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
+}
+
+struct SecureTextField: View {
+    @State var isShowPassword: Bool = true
+    @Binding var textFieldText: String
+    
+    let placeholder: String
+    
+    var body: some View {
+        HStack {
+            if isShowPassword {
+                SecureField(placeholder, text: $textFieldText)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled(true)
+            } else {
+                TextField(placeholder, text: $textFieldText)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled(true)
+            }
+            
+            Button("", systemImage: "eye") {
+                isShowPassword.toggle()
+            }
+            .foregroundStyle(.greyPrimary)
+            
+        }
     }
 }
---
+++ NewsToday/Network/ApiKey.swift
@@ -0,0 +1,13 @@
+//
+//  ApiKey.swift
+//  NewsToday
+//
+//  Created by Даниил Сивожелезов on 22.10.2024.
+//
+
+import Foundation
+
+struct ApiKey {
+    static let first = "pub_568854a7fb181713bae23f68d4a833dcaf42a"
+    static let second = "pub_570081d3db0c26842b9f6a6f0c8848ae6c110"
+}
