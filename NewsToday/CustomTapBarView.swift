da76aced93c63d7a9ef4eb707df2306fab4ccd54
denisgindulin <denisgindulin@me.com>
2024-10-23 10:44:49 +0300

    fix: reset space between the buttons of the tap bar

--- NewsToday/CustomTapBarView.swift
+++ NewsToday/CustomTapBarView.swift
@@ -19,35 +19,33 @@ struct CustomTabBarView: View {
     
     var body: some View {
         HStack {
+            Spacer()
             Button(action: { selectedTab = .home   }) { //switch to HomeView
                 Image("Home")
-//                    .foregroundColor(.blue)
                     .foregroundStyle(selectedTab != .home ? .greyLight : .purplePrimary)
             }
+            Spacer()
             
+            Spacer()
             Button(action: { selectedTab = .categories }) { //switch to CategoriesView
                 Image("categoriesIcon")
                     .foregroundStyle(selectedTab != .categories ? .greyLight : .purplePrimary)
             }
+            Spacer()
             
+            Spacer()
             Button(action: { selectedTab = .bookmark }) { //switch to BookmarksView
                 Image("bookmarkIcon")
                     .foregroundStyle(selectedTab != .bookmark ? .greyLight : .purplePrimary)
             }
+            Spacer()
             
+            Spacer()
             Button(action: { selectedTab = .profile }) { //switch to ProfileView
                 Image("profileIcon")
                     .foregroundStyle(selectedTab != .profile ? .greyLight : .purplePrimary)
             }
-            
-            /*
-            Button{
-                // switch to BookmarkView
-                selectedTab = .bookmark
-            } label: {
-                Image(selectedTab != .bookmark ? "bell" : "bellRed")
-            }
-            */
+            Spacer()
         }
     }
 }
