474428228dbbfad78b00af3947e6c669ef28cd8e
denisgindulin <denisgindulin@me.com>
2024-10-24 11:55:05 +0300

    feat: create StartView with app tilte; add Adamina font

---
+++ NewsToday/Start/StartView.swift
@@ -0,0 +1,44 @@
+//
+//  StartView.swift
+//  NewsToday
+//
+//  Created by Денис Гиндулин on 23.10.2024.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    @State private var onboardingViewsIsOn = false
+    private let textHorizontalOffsetValue = UIScreen.main.bounds.width/10
+    private let textVerticalOffsetValue = -(UIScreen.main.bounds.width/5)
+    private let additionalVerticalOffsetValue = UIScreen.main.bounds.width/20
+    private let titleFontSize: CGFloat = 60
+    
+    var body: some View {
+        ZStack {
+            Color(.purpleDarker)
+                .ignoresSafeArea()
+            VStack {
+                Text("News")
+                    .offset(x: -textHorizontalOffsetValue,
+                            y: textVerticalOffsetValue + additionalVerticalOffsetValue)
+                Text("ToDay")
+                    .offset(x: textHorizontalOffsetValue,
+                            y: textVerticalOffsetValue)
+            }
+            .font(.custom(AdaminaFont.regular.rawValue, size: titleFontSize))
+            .foregroundStyle(.greyLight)
+            .shadow(radius: 2, y: 4)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 12) {
+                self.onboardingViewsIsOn = true
+            }
+        }
+        .fullScreenCover(isPresented: $onboardingViewsIsOn) { RootView() } // FIXME: заменить RootView на первый экран онбординга
+    }
+}
+
+#Preview {
+    StartView()
+}
