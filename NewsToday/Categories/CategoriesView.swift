6bb2232eea65d75ceb7c7618aeee74f44ddd40a7
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-10-28 18:44:43 +0500

    издавился от предупреждений в коде

--- NewsToday/Categories/CategoriesView.swift
+++ NewsToday/Categories/CategoriesView.swift
@@ -15,12 +15,7 @@ struct CategoriesView: View {
     let subtitle: String
     let showButton: Bool
     
-    @State private var selectedCategories: Set<Category> = [] 
-//        didSet {
-//            print("Selected categories changed: \(selectedCategories)")
-//            viewModel.saveFavoriteCategory(Array(selectedCategories))
-//        }
-//    }
+    @State private var selectedCategories: Set<Category> = []
     
     var body: some View {
         ScrollView {
@@ -53,42 +48,4 @@ struct CategoriesView: View {
     CategoriesView(title: "Select your favorite topics",
                    subtitle: "Select some of your favorite topics to let us suggest better news for you.",
                    showButton: true)
-}
-
-struct CategoryItems: View {
-    @EnvironmentObject var authViewModel: AuthViewModel
-    @ObservedObject var viewModel: CategoriesViewModel
-    @Binding var selectedCategories: Set<Category>
-    
-    let columns: [GridItem] = [GridItem(.adaptive(minimum: 160), spacing: 16)]
-    
-    
-    var body: some View {
-        LazyVGrid(columns: columns, spacing: 16) {
-            ForEach(Category.allCases, id: \.rawValue) { category in
-                Button {
-                    toggleSelection(for: category)
-                    viewModel.saveFavoriteCategory(Array(selectedCategories))
-                } label: {
-                    HStack {
-                        CategoryEmojiView(category: category)
-                        Text(category.rawValue)
-                    }
-                    .padding(.vertical, 24)
-                    .frame(maxWidth: .infinity)
-                    .foregroundStyle(selectedCategories.contains(category) ? .white : .greyDarker)
-                    .background(selectedCategories.contains(category) ? .purplePrimary : .greyLighter)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                }
-            }
-        }
-    }
-    
-    private func toggleSelection(for category: Category) {
-        if selectedCategories.contains(category) {
-            selectedCategories.remove(category)
-        } else {
-            selectedCategories.insert(category)
-        }
-    }
 }
