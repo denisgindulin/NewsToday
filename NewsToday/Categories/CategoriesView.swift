034bbc9a0211a3a11ff47a26458f37369091be90
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-10-28 13:24:45 +0500

    сделал UI для экрана категорий и настроил вход после регистрации

--- NewsToday/Categories/CategoriesView.swift
+++ NewsToday/Categories/CategoriesView.swift
@@ -8,11 +8,72 @@
 import SwiftUI
 
 struct CategoriesView: View {
+    let title: String
+    let subtitle: String
+    let showButton: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 32) {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(title)
+                        .interFont(size: 24)
+                        .foregroundStyle(.blackPrimary)
+                    Text(subtitle)
+                        .interFont(type: .regular)
+                        .foregroundStyle(.greyPrimary)
+                }
+                VStack(alignment: .center, spacing: 16) {
+                    CategoryItems()
+                    Button {
+                        //
+                    } label: {
+                        Text("Next")
+                            .authButton()
+                    }
+
+                }
+            }
+            .padding(.horizontal, 20)
+        }
     }
 }
 
 #Preview {
-    CategoriesView()
+    CategoriesView(title: "Select your favorite topics",
+                   subtitle: "Select some of your favorite topics to let us suggest better news for you.",
+                   showButton: true)
+}
+
+struct CategoryItems: View {
+    let columns: [GridItem] = [GridItem(.adaptive(minimum: 160), spacing: 16)]
+    @State private var selectedCategories: Set<Category> = []
+    
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 16) {
+            ForEach(Category.allCases, id: \.rawValue) { category in
+                Button {
+                    toggleSelection(for: category)
+                } label: {
+                    HStack {
+                        CategoryEmojiView(category: category)
+                        Text(category.rawValue)
+                    }
+                    .padding(.vertical, 24)
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(selectedCategories.contains(category) ? .white : .greyDarker)
+                    .background(selectedCategories.contains(category) ? .purplePrimary : .greyLighter)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                }
+            }
+        }
+    }
+    
+    private func toggleSelection(for category: Category) {
+        if selectedCategories.contains(category) {
+            selectedCategories.remove(category)
+        } else {
+            selectedCategories.insert(category)
+        }
+    }
 }
---
+++ NewsToday/Network/Category.swift
@@ -0,0 +1,27 @@
+//
+//  Categories.swift
+//  NewsToday
+//
+//  Created by Даниил Сивожелезов on 22.10.2024.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case crime = "Crime"
+    case business = "Business"
+    case domestic = "Domestic"
+    case education = "Education"
+    case entertainment = "Entertainment"
+    case environment = "Environment"
+    case food = "Food"
+    case health = "Health"
+    case lifestyle = "Lifestyle"
+    case other = "Other"
+    case politics = "Politics"
+    case science = "Science"
+    case sports = "Sports"
+    case technology = "Technology"
+    case top = "Top"
+    case world = "World"
+}
